b6afea0a965c5aa71e98b5c53fe2e54a93200cbb
yeex <mozayntwali@gmail.com>
2020-07-14 00:18:10 +0200

    Error that needs to get fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule, MatProgressBarModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { FileComponent } from './file/file.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    FileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,  
+    MatIconModule,  
+    MatButtonModule,  
+    MatCardModule,  
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/file/file.component.ts
@@ -0,0 +1,62 @@
+import { UploadService } from './../upload.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-file',
+  templateUrl: './file.component.html',
+  styleUrls: ['./file.component.css']
+})
+export class FileComponent implements OnInit {
+    @ViewChild("fileUpload", {static: false}) fileUpload: ElementRef;files = [];
+  constructor(private uploadService: UploadService) { }
+
+  uploadFile(file) {  
+    const formData = new FormData();  
+    formData.append('file', file.data);  
+    file.inProgress = true;  
+    this.uploadService.upload(formData).pipe(  
+      map(event => {  
+        switch (event.type) {  
+          case HttpEventType.UploadProgress:  
+            file.progress = Math.round(event.loaded * 100 / event.total);  
+            break;  
+          case HttpEventType.Response:  
+            return event;  
+        }  
+      }),  
+      catchError((error: HttpErrorResponse) => {  
+        file.inProgress = false;  
+        return of(`${file.data.name} upload failed.`);  
+      })).subscribe((event: any) => {  
+        if (typeof (event) === 'object') {  
+          console.log(event.body);  
+        }  
+      });  
+  }
+
+  private uploadFiles() {  
+    this.fileUpload.nativeElement.value = '';  
+    this.files.forEach(file => {  
+      this.uploadFile(file);  
+    });  
+  }
+
+  onClick() {  
+    const fileUpload = this.fileUpload.nativeElement;fileUpload.onchange = () => {  
+    for (let index = 0; index < fileUpload.files.length; index++)  
+    {  
+     const file = fileUpload.files[index];  
+     this.files.push({ data: file, inProgress: false, progress: 0});  
+    }  
+      this.uploadFiles();  
+    };  
+    fileUpload.click();  
+  }
+
+  ngOnInit() {
+  }
+
+}
