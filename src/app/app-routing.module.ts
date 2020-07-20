b0d8d08ce22c7f0adc2e3be1487e6f2453f02d0a
yeex <mozayntwali@gmail.com>
2020-07-20 16:41:32 +0200

    Single tabbed Registry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent},
+  { path: '', redirectTo: 'signup', pathMatch: 'full'},
   { path: 'signup', component: SignupComponent}
 ];
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray, FormControl } from '@angular/forms'; 
 
 @Component({
   selector: 'signup',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  form;
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      fName: ['', Validators.required],
+      lName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      task: ['', Validators.required]
+    })
+  }
 
-  constructor() { }
+  get fName() { return this.form.get('fName')}
+  get lName() { return this.form.get('lName')}
+  get email() { return this.form.get('email')}
+  get task() { return this.form.get('task')}
 
   ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.form.value)
   }
 
 }
