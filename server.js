b580bc6f8d4793e4cedb748f5d9fc9eba8ec23c1
yeex <mozayntwali@gmail.com>
2020-08-16 01:46:04 +0200

    Still trying

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/qee'));
+app.use(express.static(__dirname + '/dist/QEE'));
 
 app.get('/*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/qee/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/QEE/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
